09d7c3174e8da3c207a19bf6d8c77e8c2b83a8e8
BVRXNDVRK <igxr.lxgvenyuk@gmail.com>
2024-02-01 16:25:51 +0300

    created catalog section and made it adaptive

--- src/js/script.js
+++ src/js/script.js
@@ -47,4 +47,22 @@ $(document).ready(function(){
         }, 4000)
       });
     });
+
+    $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
+      $(this)
+        .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+        .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
+    });
+
+    const toggleClass = (btnClass) => {
+      $(btnClass).each(function(i) {
+        $(this).on('click', function(e) {
+          e.preventDefault();
+          $('.catalog-item__wrapper').eq(i).toggleClass('catalog-item__wrapper_active');
+        })
+      })
+    }
+
+    toggleClass('.catalog-item__link');
+    toggleClass('.catalog-item__back');
   });
